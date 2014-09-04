ab86e183cc6dc395f3fa6891995724f52f2675cf
Stefan Boehringer <github@s-boehringer.org>
2014-09-04 12:07:18 +0200

    rename genetics_haplotype.R

--- GeneticsHaplotype.R
+++ GeneticsHaplotype.R
@@ -1,10 +1,10 @@
 #
-#	genetics_haplotype.R
+#	GeneticsHaplotype.R
 #Wed Sep  3 18:18:06 CEST 2014
 
 library('devtools');
 
 if (1) {
-	install('genetics.haplotype');
+	install('GeneticsHaplotype');
 }
 
