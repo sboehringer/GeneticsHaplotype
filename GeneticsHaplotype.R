98e1ae037cca14eb2f8112f7ce29ff581f0c88c2
Stefan Boehringer <github@s-boehringer.org>
2014-10-07 18:22:00 +0200

    working simulations + visualization

--- GeneticsHaplotype.R
+++ GeneticsHaplotype.R
@@ -4,16 +4,15 @@
 
 library('devtools');
 library('Rcpp');
-source('haplotype.R');
+source('GeneticsHaplotype/R/pedigree.R');
+source('GeneticsHaplotype/R/simulation.R');
+source('RgenericAll.R');
 
 if (F) {
 	system('rm GeneticsHaplotype/src/*.o GeneticsHaplotype/src/*.so');
 	Sys.setenv("PKG_CXXFLAGS"="-std=c++11")
 	install('GeneticsHaplotype', threads = 6);
 	require('GeneticsHaplotype');
-}
-
-if (0) {
 	M = Module('Reconstructor', PACKAGE = 'GeneticsHaplotype');
 }
 
@@ -33,11 +32,60 @@ print(hts);
 
 freqs = function(v)table(v)/sum(table(v))
 
-if (1) {
+if (0) {
 	N = 1e4;
 	hts = sapply(1:N, function(i, ped, hfs) {
 		hts = R$drawFromHfs(hfs, runif(1));
 		as.vector(hts[ped$founders + 1, ])
 	}, ped = ped1, hfs = 1:8);
 	print(freqs(as.vector(hts)));
+}
+
+if (0) {
+	ped = Df(names = c('iid', 'fid', 'mid', 'pid'), matrix(
+		c(	10, 1, NA, NA,
+			20, 1, NA, NA,
+			30, 1, 10, 20,
+			40, 1, NA, NA,
+			55, 1, 30, 40,
+
+			10, 2, NA, NA,
+			20, 2, NA, NA,
+			30, 2, 10, 20,
+			40, 2, NA, NA,
+			55, 2, 30, 40
+		)
+	, byrow = T, ncol = 4));
+	ped2 = pedSplit2ivTrios(ped);
+	s = simulateDiplotypes(ped2, hfs = 1:8);
+}
+
+if (0) {
+	pedP = ped[sample(nrow(ped), nrow(ped)), ];
+}
+if (0) {
+	print(data.frame(ped, sex = pedInferSex(ped)));
+	r = data.frame(pedP, sex = pedInferSex(pedP));
+	print(r[order(r$fid, r$iid), ]);
+}
+
+if (0) {
+	plotPedigree(ped);
+}
+
+if (1) {
+	N = 5;
+	pedTemplate = Df(names = c('iid', 'mid', 'pid'), matrix(
+		c(	1, NA, NA,
+			2, NA, NA,
+			3, 1, 2,
+			4, NA, NA,
+			5, 3, 4
+	), byrow = T, ncol = 3));
+	ped = familiesFromTemplate(pedTemplate, N = 5);
+	print(data.frame(ped, sex = pedInferSex(ped)));
+	pedSplit = pedSplit2ivTrios(ped);
+	s = simulateDiplotypes(pedSplit, hfs = 1:8);
+
+	plotPedigree(ped, tag = apply(s, 1, function(r)paste(r, collapse = '/')));
 }
